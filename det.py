3b2db26b5d0c713290d9ed0b1caa1a54a5898712
KUH-LEED <41763219+Th3P4tch@users.noreply.github.com>
2019-04-06 13:22:19 +0200

    Create det.py
    u need to install python2 . numpy , opencv

---
+++ det.py
@@ -0,0 +1,30 @@
+import numpy as np
+import cv2 as cv
+
+face_cas = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cas = cv.CascadeClassifier('haarcascade_eye.xml')
+
+cap = cv.VideoCapture(0)
+
+while True:
+ try: 
+  ret , img = cap.read()
+  gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+  faces = face_cas.detectMultiScale(gray, 1.2, 5)
+  for (x,y,w,h) in faces:
+    cv.rectangle(img,(x,y),(x+w,y+h),(230,90,61),2)
+    print("face")
+    roi_gray = gray[y:y+h,x:x+w]
+    roi_color = img[y:y+h,x:x+w]
+    eyes = eye_cas.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+      cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(61,61,230),2)
+      print("eye")
+  cv.imshow('img',img)
+  k= cv.waitKey(30) & 0xff
+  if k == 27:
+   break
+ except KeyboardInterrupt:
+  break
+cap.release()
+cv.destroyAllWindows()
